92e42b809a131fdd1a08ea49ac104a514285c55c
yurakuzo <yurakuzo20@gmail.com>
2024-12-26 22:18:37 +0000

    init project & complete task1
    Signed-off-by: yurakuzo <yurakuzo20@gmail.com>

---
+++ Make_task1/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "src1.h"
+
+int main() {
+    printf("Main function\n");
+    func1();
+    return 0;
+}
+
---
+++ Make_task1/src1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "src1.h"
+
+void func1() {
+    printf("Function 1\n");
+}
+
